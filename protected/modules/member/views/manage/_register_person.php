220bb783d15603147522bfc2c73165e70d330946
QoolAtive <QoolAtive@192.168.1.114>
2013-07-05 15:37:56 +0700

    mix modifyyy II

--- protected/modules/member/views/manage/_register_person.php
+++ protected/modules/member/views/manage/_register_person.php
@@ -396,7 +396,7 @@
                 <?php if (CCaptcha::checkRequirements()) { ?>
                     <div class="_50">
                         <?php
-                        echo "addd by mixz";
+                        echo "addd by mixer Version II";
                         echo $form->labelEx($model_user, 'verifyCode');
                         $this->widget('CCaptcha');
                         echo $form->textField($model_user, 'verifyCode');
